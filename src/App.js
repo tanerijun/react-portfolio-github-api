71bbbf7198f4f23138deea9019802e3442e543fb
TaneriX07 <tanerivince@gmail.com>
2022-05-16 11:39:29 +0800

    Add Projects and Not Found page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Profile userName="octocat" />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  const userName = 'tanerijun';
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Profile userName={userName} />} />
+          <Route path="/projects" element={<Projects userName={userName} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
