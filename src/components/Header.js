d18789454a9a0b2dcb3bb685b9d209f3f22f87cc
TaneriX07 <tanerivince@gmail.com>
2022-05-16 12:54:36 +0800

    Add link to routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import './Header.css';
 import logo from '../assets/logo.svg';
 
 function Header() {
   return (
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>React Portfolio with Github API</h2>
+      <div className="title">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React Github</h2>
+      </div>
+      <nav>
+        <RouterLink class="app-link" to="/">
+          About
+        </RouterLink>
+        <RouterLink class="app-link" to="/projects">
+          Projects
+        </RouterLink>
+      </nav>
     </div>
   );
 }
